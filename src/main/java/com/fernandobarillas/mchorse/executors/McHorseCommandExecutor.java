6cc16ba663309bba90068eaf0266740748e36bf3
Fernando Barillas <fbis251@mailbox.org>
2015-03-23 11:05:36 -0700

    Add beginning support for subcommands (Ex: /horse add)

--- src/main/java/com/fernandobarillas/mchorse/executors/McHorseCommandExecutor.java
+++ src/main/java/com/fernandobarillas/mchorse/executors/McHorseCommandExecutor.java
@@ -8,21 +8,18 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
-import java.util.HashMap;
-
 public class McHorseCommandExecutor implements CommandExecutor {
 
     private final static String PLUGIN_COMMAND_LONG_NAME = "horse";
     private final static String PLUGIN_COMMAND_SHORT_NAME = "h";
     private final Plugin mPlugin;
-    private HashMap<String, Player> playerList;
 
     public McHorseCommandExecutor(Plugin plugin) {
         mPlugin = plugin;
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] arguments) {
 
         // All plugin commands can only be sent by players
         if (!(sender instanceof Player)) {
@@ -35,21 +32,40 @@ public class McHorseCommandExecutor implements CommandExecutor {
         String commandName = command.getName();
 
         if (commandName.equalsIgnoreCase(PLUGIN_COMMAND_LONG_NAME) || commandName.equalsIgnoreCase(PLUGIN_COMMAND_SHORT_NAME)) {
-
-            if (args.length < 1) {
+            if (arguments.length < 1) {
                 MessageHelper.pluginMessage(player, "Not enough arguments");
                 return false;
             }
 
-            if (args.length > 2) {
-                MessageHelper.pluginMessage(player,"Too many arguments");
+            if (arguments.length > 2) {
+                MessageHelper.pluginMessage(player, "Too many arguments");
                 return false;
             }
 
-            // TODO: Add, remove, start, stop, score, players
-            MessageHelper.pluginMessage(player, "Starting a game of horse");
+            String commandArgument, otherPlayerName;
 
-            return true;
+            commandArgument = arguments[0];
+            if (arguments.length == 2) {
+                // TODO: Add permission for allowing players to add/remove other players
+                otherPlayerName = arguments[1];
+            }
+
+            // Handle the plugin command arguments here to allow players to add, remove, etc. themselves from the queue
+            // TODO: Add, remove, start, stop, score, players
+            switch (commandArgument) {
+                case "a":
+                case "add":
+                    MessageHelper.pluginMessage(player, "Adding yourself to the queue");
+                    // TODO: Send an error message to the player if they're already in a queue
+                    return true;
+                case "r":
+                case "remove":
+                    MessageHelper.pluginMessage(player, "Removing yourself from the queue");
+                    // TODO: Send an error message to the player if they're not in the queue
+                    return true;
+                default:
+                    return false;
+            }
         }
 
         return false;
