d652e8d136da039aef148778b4f4620bee370fe9
Fernando Barillas <fbis251@mailbox.org>
2015-03-26 16:51:15 -0700

    McHorse: Allow customizing the game word to something other than "Horse"
    Signed-off-by: Fernando Barillas <fbis251@mailbox.org>

--- src/main/java/com/fernandobarillas/mchorse/McHorse.java
+++ src/main/java/com/fernandobarillas/mchorse/McHorse.java
@@ -2,6 +2,7 @@ package com.fernandobarillas.mchorse;
 
 import com.fernandobarillas.mchorse.executors.McHorseCommandExecutor;
 import com.fernandobarillas.mchorse.listeners.McHorsePlayerListener;
+
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,8 +12,30 @@ import java.util.HashMap;
 public class McHorse extends JavaPlugin {
 
     private HashMap<String, Player> mPlayerMap;
+
     private McHorsePlayerListener mPlayerListener = new McHorsePlayerListener(this);
 
+    private String mHorseWord;
+
+    /**
+     * Gets the word that the plugin is using to keep score. This is the word "Horse" by default.
+     *
+     * @return The word that the plugin is using to keep score.
+     */
+    public String getHorseWord() {
+        return mHorseWord;
+    }
+
+    /**
+     * Allows players to customize the word to use in a game of Horse, even though traditionally "Horse" is used.
+     *
+     * @param horseWord The new word to use for scoring. Must have a length of 1 or greater
+     */
+    public void setHorseWord(String horseWord) {
+        if (horseWord.length() > 0) {
+            mHorseWord = horseWord;
+        }
+    }
     public HashMap<String, Player> getOnlinePlayers() {
         mPlayerMap = new HashMap<String, Player>();
 
@@ -35,6 +58,7 @@ public class McHorse extends JavaPlugin {
         for (Player player : mPlayerMap.values()) {
             getLogger().info(player.getName());
         }
+        mHorseWord = Constants.HORSE_WORD;
 
         getCommand("horse").setExecutor(new McHorseCommandExecutor(this));
         getCommand("h").setExecutor(new McHorseCommandExecutor(this));
