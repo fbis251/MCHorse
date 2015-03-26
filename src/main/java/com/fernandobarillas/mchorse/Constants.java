a5c77c75b9294421d2b9707aceb03e7c27533f00
Fernando Barillas <fbis251@mailbox.org>
2015-03-26 16:43:53 -0700

    Constants add new prefix, suffix and horse word to use during the game
    Signed-off-by: Fernando Barillas <fbis251@mailbox.org>

--- src/main/java/com/fernandobarillas/mchorse/Constants.java
+++ src/main/java/com/fernandobarillas/mchorse/Constants.java
@@ -7,7 +7,18 @@ import org.bukkit.Material;
  * Created by FB on 3/20/2015.
  */
 public class Constants {
+
+    // Player chat Strings
     public static final String PLUGIN_NAME = "MCHorse";
+
     public static final String MESSAGE_PREFIX = ChatColor.YELLOW + "[Horse] " + ChatColor.RESET;
+
+    public static final String LOST_PREFIX = ChatColor.RED + "";
+
+    public static final String LOST_SUFFIX = ChatColor.RESET + "";
+
+    public static final String HORSE_WORD = "HORSE"; // The word to use to keep score
+
+    // Materials used during the game
     public static final Material TARGET_BLOCK_MATERIAL = Material.SEA_LANTERN;
 }
